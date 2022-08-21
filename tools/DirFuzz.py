49ff92e390127cbd3dbe1bd16df051e3a5356052
ZacharyZcR <2903735704@qq.com>
2022-08-21 23:57:17 +0800

    dirfuzz ver0.0.1

--- tools/DirFuzz.py
+++ tools/DirFuzz.py
@@ -1,15 +1,22 @@
+from concurrent.futures import ThreadPoolExecutor
+import threading
 import requests
 
 class DirFuzz(object):
 
-    def __int__(self):
+    def __init__(self):
         self.headers = {}
         self.proxies = {
             'http:':None,
             'https:':None,
         }
-        self.timeout = 0
-        self.target = ''
+        self.timeout = 1
+        self.target = 'http://www.baidu.com/'
+        self.dictionary = ['aaa','aaa','aaa','aaa','aaa']
+        self.max_workers = 5
+
+        self.target_length_list = []
+        self.target_status_list = []
 
     def __SelectHeaders(self,headers):
         self.headers = headers
@@ -23,6 +30,12 @@ class DirFuzz(object):
     def __SelectTarget(self,target):
         self.target = target
 
+    def __SelectDictionary(self,dictionary):
+        self.dictionary = dictionary
+
+    def __SelectMaxWorkers(self,max_workers):
+        self.max_workers = max_workers
+
     def GetTargetStatusAndLength(self,param):
         try:
             req = requests.get(url =self.target + param,
@@ -34,6 +47,21 @@ class DirFuzz(object):
             return target_length,target_status
         except Exception as e:
             print(e)
+            return 'Error','Error'
 
-    def 
+    def Scan(self):
+        with ThreadPoolExecutor(max_workers = self.max_workers) as executor:
+            result = list(executor.map(self.GetTargetStatusAndLength,self.dictionary))
+        for element in result:
+            self.target_length_list.append(element[0])
+            self.target_status_list.append(element[1])
 
+    def test(self):
+        with ThreadPoolExecutor(max_workers = self.max_workers) as executor:
+            result = list(executor.map(self.GetTargetStatusAndLength,self.dictionary))
+        for element in result:
+            print(element)
+
+if __name__ == '__main__':
+    test = DirFuzz()
+    test.test()
